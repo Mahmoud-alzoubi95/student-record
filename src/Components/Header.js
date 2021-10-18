95737251d3491bce2f5d13e1361d79615a80b5fc
Mahmoud alzoubi <mahmoudalzoubi073@gmail.com>
2021-10-18 22:49:00 +0300

    add depoly dependencies

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render(){
         return(
             <div className="HeaderContainer">
-                <img className="Headerimg" src="/image/logomadrasaty.png" />
+                <img className="Headerimg" src="https://d3ekyrd5f0qkjv.cloudfront.net/sites/default/files/styles/logo/public/project_icons/logo.png" />
                 <h5>Madrasaty Students Record</h5>
             </div>
         )
